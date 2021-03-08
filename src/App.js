7c7382db22b380b39961d6a9df0d38483094ec95
Mooventhan <kmooventhan98@gmail.com>
2021-03-08 09:24:01 +0530

    broken homepage into smaller component and finished styling for the menu compnent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './components/pages/hompage/homepage.component';
 
 
 function App() {
---
+++ src/components/pages/hompage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../../directory/directory.component';
+import './homepage.styles.scss';
+
+const HomePage = ()=>(
+    <div className="Homepage">
+        <Directory/>
+    </div>
+)
+
+export default HomePage;
