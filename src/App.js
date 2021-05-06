92b8b5f19009446270be1e72df12a94ce616e680
Mooventhan <kmooventhan98@gmail.com>
2021-05-06 08:30:10 +0530

    added custom input box

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './components/pages/hompage/homepage.component';
 import { Route, Switch, } from "react-router-dom";
 import ShopPage from './components/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
       <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route  path='/shop' component={ShopPage}/>
+          <Route  path='/signin' component={SignInAndSignUpPage}/>
+
       </Switch>
       
     </div>
